d9e5706d277d63aa9d7f3f2d665c564fe0c93c84
Alireza Hazrati <alireza.hzti@gmail.com>
2019-05-15 14:25:34 +0430

    Final version

---
+++ app.js
@@ -0,0 +1,44 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let date = require(__dirname + '/date.js')
+
+let app = express()
+let items = ['hello']
+let workItems = []
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    let day = date.getDate()
+    res.render('list', {
+        listTitle: day,
+        newListItems: items
+    })
+})
+
+app.post('/', function (req, res) {
+    let item = req.body.newItem
+    if (req.body.list === 'Work') {
+        workItems.push(item)
+        res.redirect('/work')
+    } else {
+        items.push(item)
+        res.redirect('/')
+    }
+})
+
+
+app.get('/work' ,function(req, res) {
+    res.render('list', {listTitle: 'Work List', newListItems: workItems})
+})
+
+app.post('/work', function(req, res) {
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect('/work')
+})
+
+app.listen(3000, function () {
+    console.log('Application is listening on http://localhost:3000')
+})
---
+++ date.js
@@ -0,0 +1,19 @@
+function getDate() {
+    const today = new Date()
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    return  today.toLocaleDateString('en-US', options)
+}
+function getDay() {
+    const today = new Date()
+    const options = {
+        weekday: 'long'
+    }
+    return  today.toLocaleDateString('en-US', options)
+}
+
+exports.getDate = getDate
+exports.getDay = getDay
